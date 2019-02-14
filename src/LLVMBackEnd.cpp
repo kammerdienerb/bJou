815405a67b81226bc87f4242e25aafc817b4386e
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-02-14 11:23:38 -0500

    all global vars are external linkage... I just don't even care now

--- src/LLVMBackEnd.cpp
+++ src/LLVMBackEnd.cpp
@@ -887,14 +887,14 @@ static void * GenerateGlobalVariable(VariableDeclaration * var,
             llvm::Type * ll_ptr_t = llbe->getOrGenType(ptr_t);
             gvar =
                 new llvm::GlobalVariable(*llbe->llModule, ll_ptr_t, false,
-                                         llvm::GlobalVariable::InternalLinkage,
+                                         llvm::GlobalVariable::ExternalLinkage,
                                          0, var->getMangledName());
         } else {
             gvar = new llvm::GlobalVariable(
                 /*Module=*/*llbe->llModule,
                 /*Type=*/ll_t,
                 /*isConstant=*/false,
-                /*Linkage=*/llvm::GlobalValue::InternalLinkage,
+                /*Linkage=*/llvm::GlobalValue::ExternalLinkage,
                 /*Initializer=*/0, // has initializer, specified below
                 /*Name=*/var->getMangledName());
         }
@@ -915,7 +915,7 @@ static void * GenerateGlobalVariable(VariableDeclaration * var,
 
             llvm::GlobalVariable * under = new llvm::GlobalVariable(
                 *llbe->llModule, ll_t, false,
-                llvm::GlobalVariable::InternalLinkage, 0,
+                llvm::GlobalVariable::ExternalLinkage, 0,
                 "__bjou_array_under_" + var->getMangledName());
             under->setAlignment(llbe->layout->getPreferredAlignment(under));
 
